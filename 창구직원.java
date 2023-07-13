61c7010bc6a63c79970835275ca83fef507fd0cd
leewooyup <zjadlspun1114@naver.com>
2023-07-13 15:20:22 +0900

    창구직원 싱글톤 패턴

---
+++ 창구직원.java
@@ -0,0 +1,12 @@
+package hello_java_world;
+
+public class 창구직원 {
+	public static 창구직원 a창구직원;
+	private 창구직원() {}
+	public 창구직원 get창구직원() {
+		if(a창구직원 == null) {
+			a창구직원 = new 창구직원();
+		}
+		return a창구직원;
+	}
+}
